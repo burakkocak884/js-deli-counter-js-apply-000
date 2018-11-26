a04e58977e1a84fa2c5ec3d66d7ef3d645279b55
BURAK KOCAK <burakkocak884@hotmail.com>
2018-11-26 17:00:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var katzDeliLine =[]
 deliLine.push(newPerson)
 
 return "Welcome, " + newPerson + ". You are number "+ deliLine.length + " in line."
-
-
 }
+function nowServing(deliLine){
+
+  if(deliLine === 0)
+  return "There is nobody waiting to be served!"
+  else 
+    return "Currently serving "+ deliLine 
+  }
