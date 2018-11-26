8373cacef63d7db5618fdc9970f57c424b4dfabf
BURAK KOCAK <burakkocak884@hotmail.com>
2018-11-26 16:52:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine =[]
 function deli(){
   function takeANumber(deliLine, newPerson){
-    
+
 takeANumber(katzDeliLine, "Ada")
 deliLine.push(newPerson)
 return "Welcome, " + newPerson + ". You are number #"+ deliLine+ " in line"
